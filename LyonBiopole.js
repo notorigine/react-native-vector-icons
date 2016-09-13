d1b551c28f2860cf2e53cd687f76e1fcc0287149
Thomas Aufresne <thomas@rezo-zero.com>
2016-09-13 17:50:15 +0200

    Ⓐ added LyonBiopole font

---
+++ LyonBiopole.js
@@ -0,0 +1,54 @@
+/**
+ * Icomoon icon set component.
+ */
+
+import createIconSet from './lib/create-icon-set';
+const glyphMap = {
+  "icon-biopole_actualites": 61440,
+  "icon-biopole_agenda": 61441,
+  "icon-biopole_annuaire": 61442,
+  "icon-biopole_appel-projet": 61443,
+  "icon-biopole_check-mark": 61444,
+  "icon-biopole_conseil": 61445,
+  "icon-biopole_conseil_cercle": 61446,
+  "icon-biopole_contact": 61447,
+  "icon-biopole_edit": 61448,
+  "icon-biopole_en_flag": 61449,
+  "icon-biopole_entreprise": 61456,
+  "icon-biopole_entreprise_cercle": 61457,
+  "icon-biopole_etablissements": 61458,
+  "icon-biopole_etablissements_cercle": 61459,
+  "icon-biopole_fax": 61460,
+  "icon-biopole_fax_cercle": 61461,
+  "icon-biopole_fiche_entreprise": 61462,
+  "icon-biopole_fleche": 61463,
+  "icon-biopole_fr_flag": 61464,
+  "icon-biopole_linkedin": 61465,
+  "icon-biopole_mail_cercle": 61472,
+  "icon-biopole_menu": 61473,
+  "icon-biopole_moin": 61474,
+  "icon-biopole_notifications": 61475,
+  "icon-biopole_offres_biopole": 61476,
+  "icon-biopole_phone": 61477,
+  "icon-biopole_phone_cercle": 61478,
+  "icon-biopole_plus": 61479,
+  "icon-biopole_prochain_evenement": 61480,
+  "icon-biopole_profil": 61481,
+  "icon-biopole_projetRD": 61488,
+  "icon-biopole_projetRD_cercle": 61489,
+  "icon-biopole_selection": 61490,
+  "icon-biopole_structure_recherche": 61492,
+  "icon-biopole_structure_recherche_cercle": 61493,
+  "icon-biopole_triangle": 61494,
+  "icon-biopole_twitter": 61495,
+  "icon-biopole_unite_recherche": 61496,
+  "icon-biopole_unite_recherche_cercle": 61504,
+  "icon-biopole_user": 61505,
+  "icon-biopole-22": 61506
+};
+
+let LyonBiopole = createIconSet(glyphMap, 'LyonBiopole', 'LyonBiopole.ttf');
+
+module.exports = LyonBiopole;
+module.exports.glyphMap = glyphMap;
+
